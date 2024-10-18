0bdf200213f13f014350ff89f60f06913219183c
mani9203 <mana.june27.8807@outlook.jp>
2024-10-18 14:59:05 +0900

    新規登録、ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
             format: { with:EMAIL_REGEX, message: "@で入力してください" } 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
-  validates :encrypted_password, presence: true,
-            format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含め,6文字以上で設定してください'}
+  validates :password, presence: true,
+            format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含め,6文字以上で設定してください'}          
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
