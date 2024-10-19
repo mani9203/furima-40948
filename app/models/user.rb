2f39abf118525f022a88e960f4d987093d76f392
mani9203 <mana.june27.8807@outlook.jp>
2024-10-19 15:52:19 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,14 @@ class User < ApplicationRecord
 
         
   validates :nickname, presence: true
-  EMAIL_REGEX = /@.+/
-  validates :email, presence: true, uniqueness: true,
-            format: { with:EMAIL_REGEX, message: "@で入力してください" } 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i.freeze
-  validates :password, presence: true,
+  validates :password,
             format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含め,6文字以上で設定してください'}          
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
