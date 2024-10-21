13f7fecc82b05294c852bbd509388e5aa860a236
mani9203 <mana.june27.8807@outlook.jp>
2024-10-21 13:49:46 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,22 @@ class Item < ApplicationRecord
   has_one :sell
   has_one_attached :image
 
+
   validates :item_title, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :shipping_source_id, presence:true
-  validates :delivery_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_source_id, presence:true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :sells_price, presence: true
+
+ 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_day
+  belongs_to :shipping_fee
+  belongs_to :shipping_source
+  
 end
