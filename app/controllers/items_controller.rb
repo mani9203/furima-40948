ad5b682128639a8874cad861a2027a2f848c7f15
mani9203 <mana.june27.8807@outlook.jp>
2024-10-21 15:10:37 +0900

    商品出品ページを表示２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
   def index
+    @items = Item.includes(:user)
   end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item, :image).permit(:item_title, :user, :explanation, :category_id, :condition_id,:shipping_fee_id,:shipping_source_id,:delivery_day_id, :sells_price).merge(user_id: current_user.id)
+  end
+
 end
