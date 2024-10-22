bdfd9b86bcbf81b6a9cd62167d67b2c74bebf46a
mani9203 <mana.june27.8807@outlook.jp>
2024-10-22 15:55:29 +0900

    JavaScriptにて販売手数料や販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
+})
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
