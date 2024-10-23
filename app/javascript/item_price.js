a81de4495284ae0569d2fd817be4ea9928bbc936
mani9203 <mana.june27.8807@outlook.jp>
2024-10-23 14:48:23 +0900

    修正1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,6 @@ const price = () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
     const addProfitDom = document.getElementById("profit");
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :sell
   has_one_attached :image
 
 
@@ -12,15 +11,15 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_source_id, presence:true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :delivery_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :sells_price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                                          format: { with: /\A[0-9]+\z/ }
+  validates :sells_price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
- 
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_day
   belongs_to :shipping_fee
   belongs_to :shipping_source
-  
+
 end
+
